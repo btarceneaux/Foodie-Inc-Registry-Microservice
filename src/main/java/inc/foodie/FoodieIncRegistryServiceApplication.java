8787dcb59c84aafa5ae8fc0f4de410e2472e67a1
Burton Arceneaux <btarceneaux@gmail.com>
2023-11-21 20:22:54 -0800

    Added route for user microservice.

--- src/main/java/inc/foodie/FoodieIncRegistryServiceApplication.java
+++ src/main/java/inc/foodie/FoodieIncRegistryServiceApplication.java
@@ -2,8 +2,10 @@ package inc.foodie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class FoodieIncRegistryServiceApplication {
 
 	public static void main(String[] args) {
